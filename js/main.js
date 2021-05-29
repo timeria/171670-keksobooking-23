ee3463d0cf6867930a85c7a5c9ade6498f25493c
Diana <timeriadi@rambler.ru>
2021-05-29 15:06:17 +0300

    Функции

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
+// Результат: целое число из диапазона "от...до"
+function randomInteger(min, max) {
+  // случайное число от min до (max+1)
+  let rand = min + Math.random() * (max + 1 - min);
+  if( max <= min ) {
+    return false;
+  }
+  return Math.floor(rand);
+}
+randomInteger(1, 3);
 
+
+// Результат: случайное число с плавающей точкой число из диапазона "от...до"
+function randomFloat(min, max, fraction) {
+  let rand = min + Math.random() * (max + 1 - min);
+  if( max <= min ) {
+    return false;
+  }
+  return parseFloat(rand).toFixed(fraction);
+}
+
+randomFloat(1, 3, 1);
 
