0cf287adcd58458518cda1efeadcd530f956b154
Diana <timeriadi@rambler.ru>
2021-05-31 19:35:22 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
 // Результат: целое число из диапазона "от...до"
-function randomInteger(min, max) {
-  // случайное число от min до (max+1)
-  let rand = min + Math.random() * (max + 1 - min);
+function getRandomInteger(min, max) {
+  const rand = min + Math.random() * (max + 1 - min);
   if( max <= min ) {
-    return false;
+    throw new Error('getRandomInteger — максимальное число меньше или равно минимальному');
   }
   return Math.floor(rand);
 }
-randomInteger(1, 3);
+getRandomInteger(1, 3);
 
 
 // Результат: случайное число с плавающей точкой число из диапазона "от...до"
-function randomFloat(min, max, fraction) {
-  let rand = min + Math.random() * (max + 1 - min);
+function getRandomFloat(min, max, fraction) {
+  const rand = min + Math.random() * (max - min);
   if( max <= min ) {
-    return false;
+    throw new Error('getRandomInteger — максимальное число меньше или равно минимальному');
   }
   return parseFloat(rand).toFixed(fraction);
 }
 
-randomFloat(1, 3, 1);
+getRandomFloat(1.1, 2.3, 2);
 
