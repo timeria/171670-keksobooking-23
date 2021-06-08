1d388c12026b3b667a1fecd3f13abf6586fcfb99
Diana <timeriadi@rambler.ru>
2021-06-08 20:56:17 +0300

    Задание 4

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ function getRandomInteger(min, max) {
   }
   return Math.floor(rand);
 }
-getRandomInteger(1, 3);
 
 
 // Результат: случайное число с плавающей точкой число из диапазона "от...до"
@@ -18,4 +17,68 @@ function getRandomFloat(min, max, fraction) {
   return parseFloat(rand).toFixed(fraction);
 }
 
-getRandomFloat(1.1, 2.3, 2);
+const TYPE_HOUSE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECKIN_CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FACILITIES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const greatFacilities = new Array(getRandomInteger(0, 5)).fill(null).map(() => FACILITIES);
+
+const SIMILAR_WIZARD_COUNT = 10;
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInteger(0, elements.length - 1)];
+};
+
+const createObject = () => {
+  return {
+    author: {
+      avatar: 'img/avatars/user0'+ getRandomInteger(1, 8) + '.png',
+    },
+    offer: {
+      title: 'Заголовок',
+      address: '{{location.x}}, {{location.y}}',
+      price: getRandomInteger(0, 100000),
+      type: getRandomArrayElement(TYPE_HOUSE),
+      rooms: getRandomInteger(0, 10),
+      guests: getRandomInteger(0, 100),
+      checkin: getRandomArrayElement(CHECKIN_CHECKOUT),
+      checkout: getRandomArrayElement(CHECKIN_CHECKOUT),
+      features: getRandomArrayElement(FACILITIES),
+      description: 'Новое описание',
+      photos: getRandomArrayElement(PHOTOS),
+    },
+    location: {
+      lat: getRandomFloat(35.65000, 35.70000, 5),
+      lng: getRandomFloat(139.70000, 139.80000, 5),
+    }
+  };
+};
+
+const similarObject = new Array(SIMILAR_WIZARD_COUNT).fill(null).map(() => createObject());
+
+console.log(similarObject);
