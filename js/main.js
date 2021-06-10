02660593997a9d0e6c24f0247e9030d750c2be24
Diana <timeriadi@rambler.ru>
2021-06-10 22:06:52 +0300

    Правки задание 4

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
-// Результат: целое число из диапазона "от...до"
-function getRandomInteger(min, max) {
-  const rand = min + Math.random() * (max + 1 - min);
-  if( max <= min ) {
-    throw new Error('getRandomInteger — максимальное число меньше или равно минимальному');
-  }
-  return Math.floor(rand);
-}
+const OFFERS_COUNT = 10;
 
-
-// Результат: случайное число с плавающей точкой число из диапазона "от...до"
-function getRandomFloat(min, max, fraction) {
-  const rand = min + Math.random() * (max - min);
-  if( max <= min ) {
-    throw new Error('getRandomFloat — максимальное число меньше или равно минимальному');
-  }
-  return parseFloat(rand).toFixed(fraction);
-}
-
-const TYPE_HOUSE = [
+const TYPE_HOUSES = [
   'palace',
   'flat',
   'house',
@@ -25,13 +8,19 @@ const TYPE_HOUSE = [
   'hotel',
 ];
 
-const CHECKIN_CHECKOUT = [
+const CHECKOUT = [
   '12:00',
   '13:00',
   '14:00',
 ];
 
-const FACILITIES = [
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -46,39 +35,65 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-const greatFacilities = new Array(getRandomInteger(0, 5)).fill(null).map(() => FACILITIES);
-
-const SIMILAR_WIZARD_COUNT = 10;
-
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomInteger(0, elements.length - 1)];
+const LATITUDE = {
+  min: 35.65000,
+  max: 35.70000,
 };
 
-const createObject = () => {
-  return {
-    author: {
-      avatar: 'img/avatars/user0'+ getRandomInteger(1, 8) + '.png',
-    },
-    offer: {
-      title: 'Заголовок',
-      address: '{{location.x}}, {{location.y}}',
-      price: getRandomInteger(0, 100000),
-      type: getRandomArrayElement(TYPE_HOUSE),
-      rooms: getRandomInteger(0, 10),
-      guests: getRandomInteger(0, 100),
-      checkin: getRandomArrayElement(CHECKIN_CHECKOUT),
-      checkout: getRandomArrayElement(CHECKIN_CHECKOUT),
-      features: getRandomArrayElement(FACILITIES),
-      description: 'Новое описание',
-      photos: getRandomArrayElement(PHOTOS),
-    },
-    location: {
-      lat: getRandomFloat(35.65000, 35.70000, 5),
-      lng: getRandomFloat(139.70000, 139.80000, 5),
-    }
-  };
+const LONGITUDE = {
+  min: 139.70000,
+  max: 139.80000,
 };
 
-const similarObject = new Array(SIMILAR_WIZARD_COUNT).fill(null).map(() => createObject());
+const PRICE = {
+  min: 0,
+  max: 100000,
+};
+// Результат: целое число из диапазона "от...до"
+const getRandomInteger = (min, max) => {
+  const rand = min + Math.random() * (max + 1 - min);
+  if( max <= min ) {
+    throw new Error('getRandomInteger — максимальное число меньше или равно минимальному');
+  }
+  return Math.floor(rand);
+};
 
-console.log(similarObject);
+// Результат: случайное число с плавающей точкой число из диапазона "от...до"
+const getRandomFloat = (min, max, fraction) => {
+  const rand = min + Math.random() * (max - min);
+  if( max <= min ) {
+    throw new Error('getRandomFloat — максимальное число меньше или равно минимальному');
+  }
+  return parseFloat(rand).toFixed(fraction);
+};
+
+const getRandomArrayElement = (arr) => arr[getRandomInteger(0, arr.length - 1)];
+
+const getLocation =  () => ({
+  lat: getRandomFloat(LATITUDE.min, LATITUDE.max, 5),
+  lng: getRandomFloat(LONGITUDE.min, LONGITUDE.max, 5),
+});
+
+const CURRENT_LOCATION = getLocation();
+
+const generateOffer = () => ({
+  author: {
+    vatar: 'img/avatars/user0' + getRandomInteger(1, 8) + '.png',
+  },
+  offer: {
+    title: 'Заголовок',
+    address: CURRENT_LOCATION,
+    price: getRandomInteger(PRICE.min, PRICE.max),
+    type: getRandomArrayElement(TYPE_HOUSES),
+    rooms: getRandomInteger(0, 10),
+    guests: getRandomInteger(0, 100),
+    checkin: getRandomArrayElement(CHECKIN),
+    checkout: getRandomArrayElement(CHECKOUT),
+    features: FEATURES.slice(getRandomInteger(0, 2), getRandomInteger(3, 6)),
+    description: 'Новое описание',
+    photos: getRandomArrayElement(PHOTOS),
+  },
+  location: CURRENT_LOCATION,
+});
+
+const listOffer = new Array(OFFERS_COUNT).fill(null).map(generateOffer);
